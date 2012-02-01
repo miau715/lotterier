a7190e5cba6ae3f977edca507901833ca7b105f0
miau715 <miau715@techbang.com.tw>
2012-02-01 17:30:46 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotterier::Application.config.secret_token = '5042cae0335d3ed62b4fed0ef6af9868d473cf594878dd4cf43616fb90b3a6e78d6c7f23f5d751bb8d52afd80390e3261557da909c8323879a76081e3b11e900'
