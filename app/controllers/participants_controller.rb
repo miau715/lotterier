9257e38a4d608dcd43c51bf824115d0e3e4157d5
miau715 <miau715@techbang.com.tw>
2012-02-22 17:07:06 +0800

    remove useless feature from participants controller

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -7,45 +7,6 @@ class ParticipantsController < ApplicationController
     redirect_to event_path(@event)
   end
   
-  def show
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.find(params[:id])
-  end
-  
-  def new
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.build
-  end
-  
-  def create
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.build(params[:participant])
-    @participant.save
-    
-    redirect_to event_participant_path(@event, @participant)
-  end
-  
-  def edit
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.find(params[:id])
-  end
-  
-  def update
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.find(params[:id])
-    @participant.update_attributes(params[:participant])
-    
-    redirect_to event_participant_path(@event, @participant)
-  end
-  
-  def destroy
-    @event = Event.find(params[:event_id])
-    @participant = @event.participants.find(params[:id])
-    @participant.destroy
-    
-    redirect_to event_participants_url(@event)
-  end
-  
   def csv_import
     require 'csv'
     require 'fileutils'
